771e6ee92d6c318eb453f92e21844665f1e22619
Miguel Cardoso <miguel.cardoso@avanade.com>
2024-06-17 21:44:17 -0300

    feat: validation for command CRUD operations

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 builder.Services.AddMarten(config =>
 {
     config.Connection(builder.Configuration.GetConnectionString("Database")!);
