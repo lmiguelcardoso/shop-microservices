57706d91ae2ba5111def65704c909a052b34c014
Miguel Cardoso <miguel.cardoso@avanade.com>
2024-06-19 20:59:58 -0300

    feat: discount.grp implementation

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Amount=1,Description="Iphone Discount", Id= 1, ProductName="Iphone 15"},
+                new Coupon { Amount=1,Description="Samsung Discount", Id= 2, ProductName="Galaxy S24"}
+            );
+        }
+
+    }
+}
